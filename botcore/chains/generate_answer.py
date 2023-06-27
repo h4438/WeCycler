c76f9932deb2d3f86301125d38e8400ab103802f
h4438 <www.hao2912@gmail.com>
2023-06-27 21:20:33 +0700

    added: ask feature chain, generate answer chain

---
+++ botcore/chains/generate_answer.py
@@ -0,0 +1,27 @@
+GENERATE_ANSWER_CONST = \
+{"inputs":["product", "question"],
+ "outputs": {"options": """a js array of possible answers for each question. The array's length should be greater than 5."""},
+"template": """You are interesting in a {product}.
+Given a question delimited by triple backquotes.
+```
+{question}
+```
+Please generate all best 5 possible answers for the given question.
+{format_instructions}
+Choices:"""}
+
+
+from langchain.llms import BaseLLM
+from langchain import LLMChain
+import sys
+import os
+sys.path.append(f"{os.path.dirname(__file__)}/../..")
+from botcore.utils.prompt_utils import build_prompt
+
+def build_generate_answer_chain(model: BaseLLM):
+    inputs = GENERATE_ANSWER_CONST['inputs']
+    outputs = GENERATE_ANSWER_CONST['outputs']
+    template = GENERATE_ANSWER_CONST['template']
+    prompt = build_prompt(inputs, outputs, template, include_parser=True)
+    chain = LLMChain(llm=model, prompt=prompt, output_key='result')
+    return chain
