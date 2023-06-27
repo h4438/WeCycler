96d6c5b67495cce7b4fa36fdb7002f59c5407990
h4438 <www.hao2912@gmail.com>
2023-06-27 21:32:11 +0700

    fixed: ask feature seq

--- tests/ask_feature_seq.py
+++ tests/ask_feature_seq.py
@@ -3,7 +3,8 @@ import sys
 import os
 sys.path.append(f'{os.path.dirname(__file__)}/../')
 from botcore.setup import trace_ai21
-from botcore.chains.ask_feature import build_ask_feature_chain, build_generate_answer_chain
+from botcore.chains.ask_feature import build_ask_feature_chain
+from botcore.chains.generate_answer import build_generate_answer_chain
 from botcore.utils.json_parser import parse_nested_json
 
 MODEL = trace_ai21()
