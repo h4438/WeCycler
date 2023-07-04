6c7436fe4734c32e505659d84ad40f503e416feb
hien-p <hphan0390@gmail.com>
2023-07-04 17:51:23 +0700

    upload app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import redis
 
 
 sys.path.append(f'{os.path.dirname(__file__)}/../')
-from botcore.chains.qa_elec_condition import build_ask_electronic_condition_chain
+from botcore.chains.qa_feature import build_ask_feature_chain
+from botcore.chains.qa_condition import build_ask_condition_chain
 from botcore.utils.json_parser import parse_nested_json
 
 from botcore.setup import trace_ai21
@@ -60,11 +61,31 @@ def set_bg_hack_url():
          unsafe_allow_html=True
      )
     
+def customize_button():
+
+    custom_button_style = """
+                <style>
+                .row-widget.stButton {
+                width: 133px;
+                margin: 20px auto;
+                font-size: 20px;
+                }
+                
+                .row-widget.stButton button {
+                /* Add your custom styles for the button here */
+                }
+                
+                </style>
+                """
+    st.markdown(custom_button_style, unsafe_allow_html=True)
+    
+    
+    
 hide_hamburger()
 set_bg_hack_url()
 
-selected_options = option_menu(None, ["weCycler", "Feature",  "Profile", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+selected_options = option_menu(None, ["📝Analytics",  "💬chat Bot", 'Settings'], 
+    icons=['📝', '🔎', 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
         "container": {"padding": "0!important", "background-color": "#a5e69f"},
@@ -96,11 +117,12 @@ def processing(input):
 
 def chat(input_user):
     MODEL = trace_ai21()
-    ask_feature = build_ask_electronic_condition_chain(MODEL)
-    features = ask_feature({"product": input_user, "n_top": 3})
-
-    return features
-
+    # ask_feature = build_ask_electronic_condition_chain(MODEL)
+    # features = ask_feature({"product": input_user, "n_top": 4})
+    
+    ask_conditions = build_ask_feature_chain(MODEL)
+    conditions = ask_conditions({"product": input_user, "n_top": 3})
+    return conditions
 
 
 
@@ -112,8 +134,7 @@ if "boxselect" not in st.session_state:
     st.session_state.boxselect = {}
 
 
-if selected_options == 'weCycler':
-
+if selected_options == '📝Analytics':
 
     st.session_state.user_text = st.text_input("Type your message", key="user_input")
 
@@ -135,10 +156,13 @@ if selected_options == 'weCycler':
         # Routing class 
         
         if response:
-            message("I want to know more your products. Please answer this: ")
+            message("Let's learn more about your products. Please answer the following questions: ")
             list_answer = {}
+            metadata = []
+            number = st.number_input('Trash Quantity:',min_value=1,step=1)
+            
             with st.form(key='my_form'):
-                number = st.number_input('numbers:')
+                message("Features: ")
                 for i, item in enumerate(data): 
                     if item['question'] not in st.session_state.boxselect:
                         st.session_state.boxselect[item['question']] = ""
@@ -149,12 +173,142 @@ if selected_options == 'weCycler':
                         index=item['options'].index(st.session_state.boxselect[item['question']]) if st.session_state.boxselect[item['question']] in item['options'] else 0,
                         key=f"{item['question']}"
                     )    
-                    list_answer[item['question']] = st.session_state.boxselect[item['question']]
-                    #list_answer.append(st.session_state.boxselect[item['question']])
-                list_answer['number'] = number
-                if st.form_submit_button('Submit Your answer'):
+                    metadata.append(st.session_state.boxselect[item['question']])
+                
+                list_answer['name'] = st.session_state.user_text
+                list_answer['features'] = metadata
+                list_answer['Quantity'] = number
+                
+                
+                
+                
+                if st.form_submit_button('Confirm Responses'):
                     st.write(list_answer)
                     st.session_state['button'] = False
-                    
-                        
-                  
+
+
+INITIAL_MESSAGE = [
+    {"role": "user", "content": "Hi!"},
+    {
+        "role": "assistant",
+        "content": "Hi there! I'm your Recycling Assistant, designed to help manage and understand your recycling needs. I can provide insights into your waste, help update your recycling preferences, connect you with matching recycling agents, and customize settings to suit your requirements. Let's start recycling smarter together!",
+    },
+]
+
+
+import re
+import html
+
+
+def format_message(text):
+    """
+    This function is used to format the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be formatted.
+    """
+    text_blocks = re.split(r"```[\s\S]*?```", text)
+    code_blocks = re.findall(r"```([\s\S]*?)```", text)
+
+    text_blocks = [html.escape(block) for block in text_blocks]
+
+    formatted_text = ""
+    for i in range(len(text_blocks)):
+        formatted_text += text_blocks[i].replace("\n", "<br>")
+        if i < len(code_blocks):
+            formatted_text += f'<pre style="white-space: pre-wrap; word-wrap: break-word;"><code>{html.escape(code_blocks[i])}</code></pre>'
+
+    return formatted_text
+
+
+def message_func(text, is_user=False, is_df=False):
+    """
+    This function is used to display the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be displayed.
+    is_user (bool): Whether the message is from the user or not.
+    is_df (bool): Whether the message is a dataframe or not.
+    """
+    if is_user:
+        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=PastelBlue&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Tanned"
+        message_alignment = "flex-end"
+        message_bg_color = "linear-gradient(135deg, #76b31d 0%, #e8e8e8 110%)"
+        avatar_class = "user-avatar"
+        st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <div style="background: {message_bg_color}; color: black; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>
+                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+    else:
+        avatar_url = "https://www.svgrepo.com/download/276096/recycling-recycle.svg"
+        message_alignment = "flex-start"
+        message_bg_color = "#fafcfc"
+        avatar_class = "bot-avatar"
+
+        # if is_df:
+        #     st.write(
+        #         f"""
+        #             <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+        #                 <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+        #             </div>
+        #             """,
+        #         unsafe_allow_html=True,
+        #     )
+        #     st.write(text)
+        #     return
+        # else:
+        text = format_message(text)
+
+        st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+                    <div style="background: {message_bg_color}; color: black; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+
+if selected_options == '💬chat Bot':
+    
+    col1, col2 = st.columns((4, 1)) 
+    styles_content = """
+    <style> #input-container { position: fixed; bottom: 0; width: 100%; padding: 10px; background-color: white; z-index: 100; } h1 { font-family: 'Roboto Slab', serif; } .user-avatar { float: right; width: 40px; height: 40px; margin-left: 5px; margin-bottom: -10px; border-radius: 50%; object-fit: cover; } .bot-avatar { float: left; width: 40px; height: 40px; margin-right: 5px; border-radius: 50%; object-fit: cover; } </style>    
+    """
+    st.write(styles_content, unsafe_allow_html=True)
+    
+    
+    # Initialize the chat messages history
+    if "messages" not in st.session_state.keys():
+        st.session_state["messages"] = INITIAL_MESSAGE
+
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
+    with col1:
+        if prompt := st.text_input(""):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    with col2: 
+        customize_button()
+        if st.button("Reset Chat"):
+            for key in st.session_state.keys():
+                del st.session_state[key]
+            st.session_state["messages"] = INITIAL_MESSAGE
+            st.session_state["history"] = []
+        
+    for message in st.session_state.messages:
+        message_func(
+            message["content"],
+            True if message["role"] == "user" else False,
+            True if message["role"] == "data" else False,
+        )
+
+        
+    
