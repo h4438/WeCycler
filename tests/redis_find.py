67c2f1b15d940253d21505f9036627f2f1dde3a6
h4438 <www.hao2912@gmail.com>
2023-07-06 03:10:23 +0700

    added: test redis

--- tests/redis_find.py
+++ tests/redis_find.py
@@ -1,10 +1,10 @@
 import sys
 import os
 sys.path.append(f'{os.path.dirname(__file__)}/../')
-from botcore.bot_redis import RedisDB
+from botcore.redis_db import RedisDB
 
 redis_db = RedisDB()
+data = {"title": "I have an old laptop","product":"laptop", "features": ["My laptop has 4 GB RAM", "Is it function well? Well", "Does it have warranty: Yes"]}
 
-data = {"title": "I have an old phone",'product':"phone", "features": ["My phone has 4 GB RAM", "Is it function well? Well"]}
 b = redis_db.search_in_wanted(data)
 print(b)
