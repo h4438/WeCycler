bf7c54a869ef87d690ebab9b6be36fa6c309bd1e
h4438 <www.hao2912@gmail.com>
2023-07-06 07:54:13 +0700

    added: test file

--- tests/redis_find.py
+++ tests/redis_find.py
@@ -5,6 +5,6 @@ from botcore.redis_db import RedisDB
 
 redis_db = RedisDB()
 data = {"title": "I have an old laptop","product":"laptop", "features": ["My laptop has 4 GB RAM", "Is it function well? Well", "Does it have warranty: Yes"]}
-
+data = {"title": "I have a TV", "product": "TV", "features": ["Does the TV still working? Very well", "Does it have any damages? It is still fine"]}
 b = redis_db.search_in_wanted(data)
 print(b)
